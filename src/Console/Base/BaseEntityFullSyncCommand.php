eda4dd450478b8440768d820b8563ef5355625c5
dmitriy <dmitriy@singleton.ee>
2023-05-11 08:26:54 +0300

    feat(#220) - Fix of the bug with removing of synced entities

--- src/Console/Base/BaseEntityFullSyncCommand.php
+++ src/Console/Base/BaseEntityFullSyncCommand.php
@@ -5,8 +5,8 @@ namespace Amqp\Console\Base;
 use Amqp\Exceptions\ResourceGatewayConnectionException;
 use Amqp\Gateways\ResourceGatewayInterface;
 use Amqp\Repositories\CachedEntityRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Date;
 
 abstract class BaseEntityFullSyncCommand extends Command
 {
@@ -17,15 +17,33 @@ abstract class BaseEntityFullSyncCommand extends Command
     {
         $entityRepository = $this->getEntityRepository();
 
-        $syncStartTime = Date::now();
+        $lastSyncDateTime = $this->prepareLastSyncDateTime();
         foreach ($this->getResourceGateway()->getResources() as $resource) {
             $this->getEntityRepository()->save($resource);
         }
 
-        $entityRepository->deleteNotSynced($syncStartTime);
+        if (!is_null($lastSyncDateTime)) {
+            $entityRepository->deleteNotSynced($lastSyncDateTime);
+        }
     }
 
     abstract public function getResourceGateway(): ResourceGatewayInterface;
 
     abstract public function getEntityRepository(): CachedEntityRepositoryInterface;
+
+    private function prepareLastSyncDateTime(): ?Carbon
+    {
+        if (!$lastSyncDateTimeAsString = $this->getEntityRepository()->getLastSyncDateTime()) {
+            return null;
+        }
+
+        $lastSyncDateTime = Carbon::parse($lastSyncDateTimeAsString);
+        $now = Carbon::now()->setTimezone($lastSyncDateTime->getTimezone());
+
+        if ($lastSyncDateTime === $now) {
+            sleep(1);
+        }
+
+        return $lastSyncDateTime;
+    }
 }
--- src/Repositories/CachedEntityRepositoryInterface.php
+++ src/Repositories/CachedEntityRepositoryInterface.php
@@ -2,13 +2,16 @@
 
 namespace Amqp\Repositories;
 
-use Illuminate\Support\Carbon;
+
+use Carbon\Carbon;
 
 interface CachedEntityRepositoryInterface
 {
     public function save(array $resource): void;
 
     public function delete(string $id): void;
+
+    public function getLastSyncDateTime(): string;
 
     public function deleteNotSynced(Carbon $syncStartTime): void;
 }
